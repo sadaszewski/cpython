31477c9e3f15f6cd3df88ebeaa3e5bc7fb8db291
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2025-05-25 14:50:11 +0200

    Start transitioning pipeline processing to separate step.

--- Python/pipeline.c
+++ Python/pipeline.c
@@ -5,10 +5,6 @@
 #include "pycore_pystate.h"       // _PyThreadState_GET()
 #include "pycore_setobject.h"     // _PySet_NextEntry()
 
-typedef struct _PyASTOptimizeState _PyASTOptimizeState;
-
-int astfold_expr(expr_ty node_, PyArena *ctx_, _PyASTOptimizeState *state);
-
 #define EXTRAS(x) (x)->lineno, (x)->col_offset, (x)->end_lineno, (x)->end_col_offset
 
 static expr_ty leftmost_call(expr_ty e, expr_ty c) {
@@ -386,6 +382,26 @@ static bool walk_stmt(stmt_ty node, bool(*callback)(expr_ty, void*), void *userd
     return true;
 }
 
+static bool walk_module(mod_ty mod, bool(*callback)(expr_ty, void*), void *userdata, bool include_store) {
+    switch (mod->kind) {
+        case Module_kind:
+            WALK_STMT_SEQ(mod->v.Module.body);
+            break;
+        case Interactive_kind:
+            WALK_STMT_SEQ(mod->v.Interactive.body);
+            break;
+        case Expression_kind:
+            WALK(mod->v.Expression.body);
+            break;
+        case FunctionType_kind:
+            WALK_SEQ(mod->v.FunctionType.argtypes);
+            WALK(mod->v.FunctionType.returns);
+            break;
+    }
+
+    return true;
+}
+
 static bool find_placeholder_callback(expr_ty node, void *placeholder_found) {
     if (node->kind == Name_kind && _PyUnicode_EqualToASCIIString(node->v.Name.id, "_")) {
         *((bool*) placeholder_found) = true;
@@ -400,7 +416,21 @@ static bool contains_placeholder(expr_ty node) {
     return placeholder_found;
 }
 
-static int transform_pipeline(expr_ty node, PyArena *arena, _PyASTOptimizeState *state) {
+static int transform_pipeline(expr_ty node, PyArena *arena);
+
+static bool walk_replace_pipelines_callback(expr_ty node, void *arena) {
+    if (node->kind == Pipeline_kind) {
+        transform_pipeline(node, arena);
+    }
+    return true;
+}
+
+int walk_replace_pipelines(mod_ty m, PyArena *arena) {
+    walk_module(m, walk_replace_pipelines_callback, arena, true);
+    return 1;
+}
+
+static int transform_pipeline(expr_ty node, PyArena *arena) {
     expr_ty lhs = node->v.Pipeline.left;
     expr_ty rhs = node->v.Pipeline.right;
     expr_ty rhs_leftmost_call = leftmost_call(rhs, NULL);
@@ -453,10 +483,5 @@ static int transform_pipeline(expr_ty node, PyArena *arena, _PyASTOptimizeState 
         rhs_leftmost_call->v.Call.args = injected_args;
     }
 
-    return astfold_expr(lhs, arena, state);
-}
-
-
-int handle_pipeline(expr_ty node, PyArena *arena, _PyASTOptimizeState *state) {
-    return transform_pipeline(node, arena, state);
+    return walk(lhs, walk_replace_pipelines_callback, arena, true);
 }

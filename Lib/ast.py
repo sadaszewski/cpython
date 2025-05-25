a41f354dd57ed0b601641b7da4212b7e55ced630
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2025-05-25 11:30:44 +0200

    Handle pipeline in Lib/ast.py.

--- Lib/ast.py
+++ Lib/ast.py
@@ -987,6 +987,13 @@ class _Unparser(NodeVisitor):
         self.fill("nonlocal ")
         self.interleave(lambda: self.write(", "), self.write, node.names)
 
+    def visit_Pipeline(self, node):
+        self.set_precedence(_Precedence.AWAIT, node.left)
+        self.traverse(node.left)
+        self.write(" |> ")
+        self.set_precedence(_Precedence.AWAIT, node.right)
+        self.traverse(node.right)
+
     def visit_Await(self, node):
         with self.require_parens(_Precedence.AWAIT, node):
             self.write("await")

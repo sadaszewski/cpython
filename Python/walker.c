46a8536c2f2b199ef791a8652a17acf3e491ef1f
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2025-05-28 18:40:05 +0200

    Finish ast_walker().

--- Python/walker.c
+++ Python/walker.c
@@ -116,81 +116,100 @@ static bool ast_walker_expr(expr_ty node, EXTRAS) {
     switch (node->kind) {
         case BoolOp_kind:
             res = ast_walker_expr_seq(node->v.BoolOp.values, EXTRA_3);
+            break;
         case NamedExpr_kind:
             res = (
                 ast_walker_expr(node->v.NamedExpr.target, Store, EXTRA_2) &&
                 ast_walker_expr(node->v.NamedExpr.value, EXTRA_3)
             );
+            break;
         case BinOp_kind:
             res = (
                 ast_walker_expr(node->v.BinOp.left, EXTRA_3) &&
                 ast_walker_expr(node->v.BinOp.right, EXTRA_3)
             );
+            break;
         case UnaryOp_kind:
             res = ast_walker_expr(node->v.UnaryOp.operand, EXTRA_3);
+            break;
         case Lambda_kind:
             res = (
                 ast_walker_arguments(node->v.Lambda.args, EXTRA_3) &&
                 ast_walker_expr(node->v.Lambda.body, EXTRA_3)
-            ); 
+            );
+            break;
         case IfExp_kind:
             res = (
                 ast_walker_expr(node->v.IfExp.test, EXTRA_3) &&
                 ast_walker_expr(node->v.IfExp.body, EXTRA_3) &&
                 ast_walker_expr(node->v.IfExp.orelse, EXTRA_3)
             );
+            break;
         case Dict_kind:
             res = (
                 ast_walker_expr_seq(node->v.Dict.keys, EXTRA_3) &&
                 ast_walker_expr_seq(node->v.Dict.values, EXTRA_3)
             );
+            break;
         case Set_kind:
             res = ast_walker_expr_seq(node->v.Set.elts, EXTRA_3);
+            break;
         case ListComp_kind:
             res = (
                 ast_walker_expr(node->v.ListComp.elt, EXTRA_3) &&
                 ast_walker_compr_seq(node->v.ListComp.generators, EXTRA_3)
             );
+            break;
         case SetComp_kind:
             res =(
                 ast_walker_expr(node->v.SetComp.elt, EXTRA_3) &&
                 ast_walker_compr_seq(node->v.SetComp.generators, EXTRA_3)
             );
+            break;
         case DictComp_kind:
             res = (
                 ast_walker_expr(node->v.DictComp.key, EXTRA_3) &&
                 ast_walker_expr(node->v.DictComp.value, EXTRA_3) &&
                 ast_walker_compr_seq(node->v.DictComp.generators, EXTRA_3)
             );
+            break;
         case GeneratorExp_kind:
             res = (
                 ast_walker_expr(node->v.GeneratorExp.elt, EXTRA_3) &&
                 ast_walker_compr_seq(node->v.GeneratorExp.generators, EXTRA_3)
             );
+            break;
         case Await_kind:
             res = ast_walker_expr(node->v.Await.value, EXTRA_3);
+            break;
         case Yield_kind:
             res = ast_walker_expr(node->v.Yield.value, EXTRA_3);
+            break;
         case YieldFrom_kind:
             res = ast_walker_expr(node->v.YieldFrom.value, EXTRA_3);
+            break;
         case Compare_kind:
             res = (
                 ast_walker_expr(node->v.Compare.left, EXTRA_3) &&
                 ast_walker_expr_seq(node->v.Compare.comparators, EXTRA_3)
             );
+            break;
         case Call_kind:
             res = (
                 ast_walker_expr(node->v.Call.func, EXTRA_3) &&
                 ast_walker_expr_seq(node->v.Call.args, EXTRA_3) &&
                 ast_walker_keyword_seq(node->v.Call.keywords, EXTRA_3)
             );
+            break;
         case FormattedValue_kind:
             res =(
                 ast_walker_expr(node->v.FormattedValue.format_spec, EXTRA_3) &&
                 ast_walker_expr(node->v.FormattedValue.value, EXTRA_3)
             );
+            break;
         case JoinedStr_kind:
             res = ast_walker_expr_seq(node->v.JoinedStr.values, EXTRA_3);
+            break;
         case Constant_kind:
             break;
         case Attribute_kind:
@@ -198,30 +217,38 @@ static bool ast_walker_expr(expr_ty node, EXTRAS) {
                 ast_walker_expr(node->v.Attribute.value, node->v.Attribute.ctx, EXTRA_2) &&
                 ast_walker_identifier(node->v.Attribute.attr, node->v.Attribute.ctx, EXTRA_2)
             );
+            break;
         case Subscript_kind:
             res = (
                 ast_walker_expr(node->v.Subscript.value, node->v.Subscript.ctx, EXTRA_2) &&
                 ast_walker_expr(node->v.Subscript.slice, node->v.Subscript.ctx, EXTRA_2)
             );
+            break;
         case Starred_kind:
             res = ast_walker_expr(node->v.Starred.value, node->v.Starred.ctx, EXTRA_2);
+            break;
         case Name_kind:
             res = ast_walker_identifier(node->v.Name.id, node->v.Name.ctx, EXTRA_2);
+            break;
         case List_kind:
             res = ast_walker_expr_seq(node->v.List.elts, node->v.List.ctx, EXTRA_2);
+            break;
         case Tuple_kind:
             res = ast_walker_expr_seq(node->v.Tuple.elts, node->v.Tuple.ctx, EXTRA_2);
+            break;
         case Slice_kind:
             res = (
                 ast_walker_expr(node->v.Slice.lower, EXTRA_3) &&
                 ast_walker_expr(node->v.Slice.step, EXTRA_3) &&
                 ast_walker_expr(node->v.Slice.upper, EXTRA_3)
             );
+            break;
         case Pipeline_kind:
             res = (
                 ast_walker_expr(node->v.Pipeline.left, EXTRA_3) &&
                 ast_walker_expr(node->v.Pipeline.right, EXTRA_3)
             );
+            break;
     }
 
     walk_node_ty walk_node = { .Expr = node };
@@ -420,6 +447,7 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
                 ast_walker_expr(node->v.FunctionDef.returns, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.FunctionDef.body, EXTRA_3)
             );
+            break;
         case AsyncFunctionDef_kind:
             res = (
                 ast_walker_expr_seq(node->v.AsyncFunctionDef.decorator_list, EXTRA_3) &&
@@ -429,6 +457,7 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
                 ast_walker_expr(node->v.AsyncFunctionDef.returns, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.AsyncFunctionDef.body, EXTRA_3)
             );
+            break;
         case ClassDef_kind:
             res = (
                 ast_walker_expr_seq(node->v.ClassDef.decorator_list, EXTRA_3) &&
@@ -438,32 +467,39 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
                 ast_walker_keyword_seq(node->v.ClassDef.keywords, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.ClassDef.body, EXTRA_3)
             );
+            break;
         case Return_kind:
             res = ast_walker_expr(node->v.Return.value, EXTRA_3);
+            break;
         case Delete_kind:
             res = ast_walker_expr_seq(node->v.Delete.targets, EXTRA_3);
+            break;
         case Assign_kind:
             res = (
                 ast_walker_expr_seq(node->v.Assign.targets, EXTRA_3) &&
                 ast_walker_expr(node->v.Assign.value, EXTRA_3)
             );
+            break;
         case TypeAlias_kind:
             res = (
                 ast_walker_expr(node->v.TypeAlias.name, EXTRA_3) &&
                 ast_walker_type_param_seq(node->v.TypeAlias.type_params, EXTRA_3) &&
                 ast_walker_expr(node->v.TypeAlias.value, EXTRA_3)
             );
+            break;
         case AugAssign_kind:
             res = (
                 ast_walker_expr(node->v.AugAssign.target, EXTRA_3) &&
                 ast_walker_expr(node->v.AugAssign.value, EXTRA_3)
             );
+            break;
         case AnnAssign_kind:
             res = (
                 ast_walker_expr(node->v.AnnAssign.target, EXTRA_3) &&
                 ast_walker_expr(node->v.AnnAssign.annotation, EXTRA_3) &&
                 ast_walker_expr(node->v.AnnAssign.value, EXTRA_3)
             );
+            break;
         case For_kind:
             res = (
                 ast_walker_expr(node->v.For.target, EXTRA_3) &&
@@ -471,6 +507,7 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
                 ast_walker_stmt_seq(node->v.For.body, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.For.orelse, EXTRA_3)
             );
+            break;
         case AsyncFor_kind:
             res = (
                 ast_walker_expr(node->v.AsyncFor.target, EXTRA_3) &&
@@ -478,38 +515,45 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
                 ast_walker_stmt_seq(node->v.AsyncFor.body, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.AsyncFor.orelse, EXTRA_3)
             );
+            break;
         case While_kind:
             res = (
                 ast_walker_expr(node->v.While.test, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.While.body, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.While.orelse, EXTRA_3)
             );
+            break;
         case If_kind:
             res = (
                 ast_walker_expr(node->v.If.test, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.If.body, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.If.orelse, EXTRA_3)
             );
+            break;
         case With_kind:
             res = (
                 ast_walker_withitem_seq(node->v.With.items, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.With.body, EXTRA_3)
             );
+            break;
         case AsyncWith_kind:
             res = (
                 ast_walker_withitem_seq(node->v.AsyncWith.items, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.AsyncWith.body, EXTRA_3)
             );
+            break;
         case Match_kind:
             res = (
                 ast_walker_expr(node->v.Match.subject, EXTRA_3) &&
                 ast_walker_matchcase_seq(node->v.Match.cases, EXTRA_3)
             );
+            break;
         case Raise_kind:
             res = (
                 ast_walker_expr(node->v.Raise.exc, EXTRA_3) &&
                 ast_walker_expr(node->v.Raise.cause, EXTRA_3)
             );
+            break;
         case Try_kind:
             res = (
                 ast_walker_stmt_seq(node->v.Try.body, EXTRA_3) &&
@@ -517,6 +561,7 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
                 ast_walker_stmt_seq(node->v.Try.orelse, EXTRA_3) &&
                 ast_walker_stmt_seq(node->v.Try.finalbody, EXTRA_3)
             );
+            break;
         case TryStar_kind:
             res = (
                 ast_walker_stmt_seq(node->v.TryStar.body, EXTRA_3) &&
@@ -530,21 +575,27 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
                 ast_walker_expr(node->v.Assert.test, EXTRA_3) &&
                 ast_walker_expr(node->v.Assert.msg, EXTRA_3)
             );
+            break;
         case Import_kind:
             res = ast_walker_alias_seq(node->v.Import.names, EXTRA_3);
+            break;
         case ImportFrom_kind:
             res = (
                 ast_walker_identifier(node->v.ImportFrom.module, EXTRA_3) &&
                 ast_walker_alias_seq(node->v.ImportFrom.names, EXTRA_3)
             );
+            break;
         case Global_kind:
             res = ast_walker_identifier_seq(node->v.Global.names, EXTRA_3);
+            break;
         case Nonlocal_kind:
             res = ast_walker_identifier_seq(node->v.Nonlocal.names, EXTRA_3);
+            break;
         case Expr_kind:
             res = (
                 ast_walker_expr(node->v.Expr.value, EXTRA_3)
             );
+            break;
         case Pass_kind:
             break;
         case Break_kind:
@@ -559,6 +610,32 @@ static bool ast_walker_stmt(stmt_ty node, EXTRAS) {
     return res;
 }
 
+static bool ast_walker_mod(mod_ty mod, EXTRAS) {
+    CHECK_NULL(mod);
+    bool res = true;
+    switch (mod->kind) {
+        case Module_kind:
+            res = ast_walker_stmt_seq(mod->v.Module.body, EXTRA_3);
+            break;
+        case Interactive_kind:
+            res = ast_walker_stmt_seq(mod->v.Interactive.body, EXTRA_3);
+            break;
+        case Expression_kind:
+            res = ast_walker_expr(mod->v.Expression.body, EXTRA_3);
+            break;
+        case FunctionType_kind:
+            res = (
+                ast_walker_expr_seq(mod->v.FunctionType.argtypes, EXTRA_3) &&
+                ast_walker_expr(mod->v.FunctionType.returns, EXTRA_3)
+            );
+            break;
+    }
+
+    walk_node_ty walk_node = { .Mod = mod };
+    res &= callback(WalkMod_kind, walk_node, ctx, userdata);
+    return res;
+}
+
 static bool ast_walker(walk_kind_ty kind, walk_node_ty node, EXTRAS) {
     switch(kind) {
     case WalkExpr_kind:
@@ -569,9 +646,9 @@ static bool ast_walker(walk_kind_ty kind, walk_node_ty node, EXTRAS) {
         return ast_walker_stmt(node.Stmt, EXTRA_3);
     case WalkMod_kind:
         CHECK_NULL(node.Mod);
-        return true;
+        return ast_walker_mod(node.Mod, EXTRA_3);
     case WalkIdentifier_kind:
         CHECK_NULL(node.Identifier);
-        return true;
+        return ast_walker_identifier(node.Identifier, EXTRA_3);
     }
 }
